d54b594255a774b555a55e33bb49ab481dc98686
RIYA AGRAHARI <RIYA AGRAHARI@10.12.24.242>
2018-02-19 19:37:24 -0800

    Added try catch block in Exception1 class for exception handling

--- CCVT_Code_Distribute/src/com/rt/exceptionexamples/Exception1.java
+++ CCVT_Code_Distribute/src/com/rt/exceptionexamples/Exception1.java
@@ -2,7 +2,14 @@ package com.rt.exceptionexamples;
 
 class Exception1 {
 public static void main(String args[]) {
+  try{
 int d = 0;
 int a = 42 / d;
+    
+  }
+  catch(ArithmeticException e){
+    System.out.println("Error:Division By zero not possible");
+    e.printStackTrace(); //for displaying error message(debugging option for developer)
+  }
 }
 }
